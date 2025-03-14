9407633c835eda8bd7eb890f3449047e5fcd338b
Ishnol <ekizzie1@students.towson.edu>
2025-03-14 19:43:16 -0400

    Update App.java

--- App.java
+++ App.java
@@ -9,11 +9,15 @@ class GameManager {
     private GameState gameState;
     private ChoiceSystem choiceSystem;
     private CutsceneManager cutsceneManager;
+    private Player player;
+    private Location currentLocation;
 
     public GameManager() {
         gameState = new GameState();
         choiceSystem = new ChoiceSystem();
         cutsceneManager = new CutsceneManager();
+        player = new Player("Elara");
+        currentLocation = new Location("Royal Palace");
     }
 
     public void startGame() {
@@ -23,7 +27,8 @@ class GameManager {
     }
 
     private void runGameLoop() {
-        // Core game loop handling player input and progression
+        System.out.println("Welcome to the game, " + player.getName() + "!");
+        System.out.println("You are currently at " + currentLocation.getName());
     }
 }
 
@@ -42,5 +47,49 @@ class ChoiceSystem {
 class CutsceneManager {
     public void playIntroCutscene() {
         System.out.println("A long time ago, in the kingdom of Aridia...");
+    }
+}
+
+class Entity {
+    protected String name;
+    protected int health;
+
+    public Entity(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Player extends Entity {
+    public Player(String name) {
+        super(name, 100);
+    }
+}
+
+class Location {
+    private String name;
+
+    public Location(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Item {
+    private String itemName;
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemName() {
+        return itemName;
     }
 }
