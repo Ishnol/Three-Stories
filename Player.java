90aafeca4f235dab3b907638c80abdc2dfc48354
Ishnol <ekizzie1@students.towson.edu>
2025-10-02 18:03:38 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,9 +4,11 @@ public class Player extends Character {
     // === Core Attributes ===
     private String elementalAffinity;
     private int sanity;
+
+    // === Modular Managers ===
     private InventoryManager inventoryManager;
-    private Map<String, Integer> relationships;
-    private Set<String> conditions = new HashSet<>();
+    private RelationshipManager relationshipManager;
+    private ConditionManager conditionManager;
 
     // === Constructor ===
     public Player(String name, int health, int mana, int sanity, int attackPower, int defense, String elementalAffinity) {
@@ -14,7 +16,8 @@ public class Player extends Character {
         this.elementalAffinity = elementalAffinity;
         this.sanity = sanity;
         this.inventoryManager = new InventoryManager();
-        this.relationships = new HashMap<>();
+        this.relationshipManager = new RelationshipManager();
+        this.conditionManager = new ConditionManager();
     }
 
     // === Getters & Setters ===
@@ -40,6 +43,15 @@ public class Player extends Character {
         return inventoryManager;
     }
 
+    public RelationshipManager getRelationshipManager() {
+        return relationshipManager;
+    }
+
+    public ConditionManager getConditionManager() {
+        return conditionManager;
+    }
+
+    // === Sanity & Conditions ===
     public boolean isSane() {
         return sanity >= 30;
     }
@@ -47,4 +59,49 @@ public class Player extends Character {
     public void checkSanityEffects() {
         if (sanity < 30) {
             System.out.println("Your mind trembles. Visions blur and commands may misfire.");
-        } else if
+        } else if (sanity > 80) {
+            System.out.println("Your foresight sharpens. You feel attuned to the desert's whispers.");
+        }
+    }
+
+    public boolean canPerformRitual() {
+        return isSane() && !conditionManager.hasCondition("cursed");
+    }
+
+    // === Combat & Synergy ===
+    public boolean isElementCompatible(String element) {
+        return this.elementalAffinity.equalsIgnoreCase(element);
+    }
+
+    public void triggerAllyCombo(String allyName) {
+        if (relationshipManager.isTrustedBy(allyName)) {
+            System.out.println("You and " + allyName + " perform a synchronized elemental strike!");
+            // Add combo logic here
+        } else {
+            System.out.println(allyName + " hesitates. Trust must be earned.");
+        }
+    }
+
+    // === Narrative Hooks ===
+    public void checkNarrativeTriggers() {
+        if (sanity <= 10) {
+            System.out.println("A dark vision overtakes you. The Entity whispers your name...");
+            // Trigger hallucination event
+        }
+        if (relationshipManager.getRelationshipLevel("Helio") >= 90 &&
+            inventoryManager.hasItem("Echo Vial")) {
+            System.out.println("Helio shares a forgotten mantra. A hidden path opens.");
+            // Unlock secret location or ritual
+        }
+    }
+
+    // === Display ===
+    public void displayStats() {
+        super.displayStats();
+        System.out.println("Elemental Affinity: " + elementalAffinity);
+        System.out.println("Sanity Level: " + sanity);
+        inventoryManager.displayInventory();
+        relationshipManager.displayRelationships();
+        conditionManager.displayConditions();
+    }
+}
