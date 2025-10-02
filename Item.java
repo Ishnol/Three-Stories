4e99392aaf39ab5660cfd5c5645c0b71670bade9
Ishnol <ekizzie1@students.towson.edu>
2025-10-02 18:29:48 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,14 +1,20 @@
-public class Item {
-    private String name; // Item name
-    private String description; // Description of the item
-    private ItemType type; // Type of item (healing, quest, etc.)
-    private int powerBoost; // Any boost it provides (e.g., mana, attack, etc.)
+import java.util.*;
 
-    public Item(String name, String description, ItemType type, int powerBoost) {
+public class Item {
+    private String name;
+    private String description;
+    private ItemType type;
+    private int powerBoost;
+    private boolean consumable;
+    private List<String> effects;
+
+    public Item(String name, String description, ItemType type, int powerBoost, boolean consumable, List<String> effects) {
         this.name = name;
         this.description = description;
         this.type = type;
         this.powerBoost = powerBoost;
+        this.consumable = consumable;
+        this.effects = effects != null ? effects : new ArrayList<>();
     }
 
     // Getters
@@ -28,6 +34,14 @@ public class Item {
         return powerBoost;
     }
 
+    public boolean isConsumable() {
+        return consumable;
+    }
+
+    public List<String> getEffects() {
+        return effects;
+    }
+
     // Display item details
     public void displayItemInfo() {
         System.out.println("Name: " + name);
@@ -35,6 +49,15 @@ public class Item {
         System.out.println("Type: " + type);
         if (powerBoost > 0) {
             System.out.println("Power Boost: " + powerBoost);
+        }
+        if (consumable) {
+            System.out.println("This item will be consumed upon use.");
+        }
+        if (!effects.isEmpty()) {
+            System.out.println("Effects:");
+            for (String effect : effects) {
+                System.out.println("- " + effect);
+            }
         }
         if (type == ItemType.KEY) {
             System.out.println("This is a key item for unlocking story progression.");
@@ -48,7 +71,9 @@ public class Item {
             System.out.println(target.getName() + " recovers " + powerBoost + " health!");
         } else if (type == ItemType.BOOST) {
             System.out.println("Boost item used, providing temporary enhancements!");
-            // You can extend this to apply buffs later
+            // Future: Apply buffs based on effects
+        } else if (type == ItemType.KEY) {
+            System.out.println("Key item activated. It may unlock a story event or puzzle.");
         } else {
             System.out.println("This item cannot be used directly.");
         }
