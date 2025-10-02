8e4a4ce3c24ae52b15bd28699c9f7d8049b1a2e0
Ishnol <ekizzie1@students.towson.edu>
2025-10-02 18:49:04 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -6,6 +6,7 @@ public class Character {
     protected int sanity; 
     protected int attackPower;
     protected int defense;
+    protected boolean onCooldown = false;
 
     public Character(String name, int health, int mana, int sanity, int attackPower, int defense) {
         this.name = name;
@@ -49,6 +50,13 @@ public class Character {
     public void heal(int amount) {
         health += amount;
     }
+    public boolean isOnCooldown() {
+    return onCooldown;
+}
+
+public void setCooldown(boolean status) {
+    this.onCooldown = status;
+}
    
     public void displayStats() {
         System.out.println("Name: " + name);
@@ -59,19 +67,32 @@ public class Character {
 }
 
 // Queen Elara Subclass
-class QueenElara extends Character {
-    private String elementalAffinity; // Fire, Water, Earth, Air
-    private boolean crownOfForesight; // Special item
+public class QueenElara extends Character {
+    private String elementalAffinity;
+    private boolean crownOfForesight;
 
     public QueenElara(String name, int health, int mana, int sanity, int attackPower, int defense, String elementalAffinity) {
         super(name, health, mana, sanity, attackPower, defense);
         this.elementalAffinity = elementalAffinity;
-        this.crownOfForesight = false; // Starts without the crown
+        this.crownOfForesight = false;
     }
 
     public void equipCrown() {
         this.crownOfForesight = true;
         System.out.println(name + " equips the Crown of Foresight!");
+    }
+
+    public void unequipCrown() {
+        this.crownOfForesight = false;
+        System.out.println(name + " removes the Crown of Foresight.");
+    }
+
+    public boolean hasCrownEquipped() {
+        return crownOfForesight;
+    }
+
+    public String getElementalAffinity() {
+        return elementalAffinity;
     }
 
     public void useElementalPower(String element) {
@@ -93,8 +114,9 @@ class QueenElara extends Character {
 
 
 
+
 // Helio Subclass
-class Helio extends Character {
+public class Helio extends Character {
     private int archerySkill;
 
     public Helio(String name, int health, int mana, int sanity, int attackPower, int defense, int archerySkill) {
@@ -105,6 +127,7 @@ class Helio extends Character {
     public int performArrowStrike() {
         int damage = attackPower + archerySkill;
         System.out.println(name + " performs an arrow strike for " + damage + " damage!");
+        this.onCooldown = true;
         return damage;
     }
 
@@ -115,11 +138,17 @@ class Helio extends Character {
     public void setArcherySkill(int archerySkill) {
         this.archerySkill = archerySkill;
     }
+
+    @Override
+    public void displayStats() {
+        super.displayStats();
+        System.out.println("Archery Skill: " + archerySkill);
+    }
 }
-  
+
 // Mylo Subclass
-class Mylo extends Character {
-    private String knowledgeSpecialty; // E.g., "Maps", "Artifacts"
+public class Mylo extends Character {
+    private String knowledgeSpecialty;
 
     public Mylo(String name, int health, int mana, int sanity, int attackPower, int defense, String knowledgeSpecialty) {
         super(name, health, mana, sanity, attackPower, defense);
@@ -128,5 +157,16 @@ class Mylo extends Character {
 
     public void revealSecret() {
         System.out.println(name + " uses their knowledge of " + knowledgeSpecialty + " to uncover hidden truths!");
+        this.onCooldown = true;
+    }
+
+    public String getKnowledgeSpecialty() {
+        return knowledgeSpecialty;
+    }
+
+    @Override
+    public void displayStats() {
+        super.displayStats();
+        System.out.println("Knowledge Specialty: " + knowledgeSpecialty);
     }
 }
