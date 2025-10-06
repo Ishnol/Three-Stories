90dd2ed672fd551d7350885b53edbddf45360001
Ishnol <ekizzie1@students.towson.edu>
2025-10-05 23:03:33 -0400

    Update Mylo.java

--- Mylo.java
+++ Mylo.java
@@ -1,1 +1,24 @@
+public class Mylo extends Character {
+    private String knowledgeSpecialty;
+
+    public Mylo(String name, int health, int mana, int sanity, int attackPower, int defense, String knowledgeSpecialty) {
+        super(name, health, mana, sanity, attackPower, defense);
+        this.knowledgeSpecialty = knowledgeSpecialty;
+    }
+
+    public void revealSecret() {
+        System.out.println(name + " uses their knowledge of " + knowledgeSpecialty + " to uncover hidden truths!");
+        this.onCooldown = true;
+    }
+
+    public String getKnowledgeSpecialty() {
+        return knowledgeSpecialty;
+    }
+
+    @Override
+    public void displayStats() {
+        super.displayStats();
+        System.out.println("Knowledge Specialty: " + knowledgeSpecialty);
+    }
+}
 
